2ca4acd95a9bb24ab60d76a50b5d746df70cae87
alexmalkov <malkov2013@gmail.com>
2013-01-12 21:13:21 +0200

    Clear.bat edited

--- 1_Clear.bat
+++ 1_Clear.bat
@@ -3,5 +3,5 @@ cd bin
 del libEngine.a
 del Engine.dll
 cd ../SourceFiles
-del CommandFactory.o Game.o GameObject.o Gold.o GoldFactory.o MoveCommand.o Player.o Room.o adventure.o AttackCommand.o
+del CommandFactory.o Game.o GameObject.o Gold.o GoldFactory.o MoveCommand.o Player.o Room.o adventure.o AttackCommand.o Engine.a
 
